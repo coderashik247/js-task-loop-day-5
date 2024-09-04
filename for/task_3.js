894d53369990d574ad07811ddd67c5b5aa7e7c12
Ashik <ashikurashik20999@gmail.com>
2024-09-05 01:12:15 +0600

    task_3 added

---
+++ for/task_3.js
@@ -0,0 +1,24 @@
+// subtask--1
+/* 
+
+let sum = 0;
+
+for(let i=81; i<=131; i++){
+    if(i%2 !== 0){
+        sum += i;
+    }
+}
+console.log("Sum: ", sum);
+
+*/
+
+
+// subtask--2
+let sum = 0;
+
+for(let i=206; i<=311; i++){
+    if(i%2 === 0){
+        sum += i;
+    }
+}
+console.log("Sum: ", sum);
---
+++ while/task_3.js
@@ -0,0 +1,24 @@
+// let i = 81;
+// let sum = 0;
+
+// while(i<=131){
+//     if(i%2 !== 0){
+//         sum += i;
+//     }
+//     i++;
+// }
+// console.log("Sum: ", sum);
+
+
+// subtask--2
+
+let sum = 0;
+let i = 206;
+
+while(i<=311){
+    if(i%2 === 0){
+        sum += i;
+    }
+    i++
+}
+console.log("Sum: ", sum);
