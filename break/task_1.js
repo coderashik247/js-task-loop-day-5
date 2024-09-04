127e1fd88f9360f6091da3893a05e35a06ea2f61
Ashik <ashikurashik20999@gmail.com>
2024-09-05 02:45:30 +0600

    task_1 added

---
+++ break/task_1.js
@@ -0,0 +1,17 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+
+let n = 1;
+
+while(n <= 200){
+    
+    if(n === 101){
+        break;
+    }
+    console.log(n);
+    n++;
+}
