086cca40a6c9d89dc1eea26e93821adb0d1e0134
Ashik <ashikurashik20999@gmail.com>
2024-09-04 20:00:53 +0600

    second commit

---
+++ for/task_1.js
@@ -0,0 +1,13 @@
+// using for loop
+
+for(let i=1; i<=60; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
+
+// While loop
+
+let i =1; 
+while(i<=60){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    i++;
+}
---
+++ for/task_2.js
@@ -0,0 +1,51 @@
+// subtask-1
+
+/* 
+
+for(let i=61; i<=100; i+=2){
+    console.log(i);
+}
+
+console.log("Another Process");
+
+for(let i=61; i<=100; i++){
+    if(i%2 === 1){
+        console.log(i);
+    }
+} 
+
+// using while loop
+
+let i=61;
+while( i<=100){
+    console.log(i);
+    i+=2
+}
+
+console.log("Another Process");
+
+let n =61;
+
+while(i<=100){
+    if(i%2 === 1){
+        console.log(i);
+    }
+    i++;
+}
+
+*/
+
+
+// subtask-2
+// Find all the even numbers from 78 to 98.
+
+for(let i=78; i<=98; i+=2){
+    // console.log(i);
+}
+
+for(let i=78; i<=98; i++){
+    if(i% 2 !== 1){
+        console.log(i);
+    }
+    i++;
+}
